2b784566bb1a482a00433458febd8481f4bcd2e3
Dabanana <j.platypus26@gmail.com>
2016-10-22 09:17:49 +1300

    Add files via upload

---
+++ Tictactoe.py
@@ -0,0 +1,104 @@
+class Tictactoe:
+
+	def __init__(self):
+		self.board = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
+		self.turn = 1
+		self.win = 0
+
+
+	def __str__(self):
+		gameboard = []
+		for row in self.board:
+			for value in row:
+				if value == 0:
+					gameboard.append(":white_medium_square:")
+				elif value == -1:
+					gameboard.append(":x:")
+				elif value == 1:
+					gameboard.append(":o:")
+		return "{0[0]} | {0[1]} | {0[2]}\n-----------------\n{0[3]} | {0[4]} | {0[5]}\n-----------------\n{0[6]} | {0[7]} | {0[8]}".format(gameboard)
+
+	def column(self):
+		checkcolumn = [0,0,0]
+		for row in self.board:
+			for (i, value) in enumerate(row):
+				checkcolumn[i] += value
+		for total in checkcolumn:
+			if total == 3:
+				self.reset_board()
+				self.win += 1
+			elif total == -3:
+				self.reset_board()
+				self.win -= 1
+
+	def row(self):
+		checkrow = [0,0,0]
+		for (i,row) in enumerate(self.board):
+			for value in row:
+				checkrow[i] += value
+		for total in checkrow:
+			if total == 3:
+				self.reset_board()
+				self.win += 1
+			elif total == -3:
+				self.reset_board()
+				self.win -= 1
+
+	def diagonal(self):
+		new = []
+		diagonal = [0,0]
+		for row in self.board:
+			for column in row:
+				new.append(column)
+		diagonal[0] = new[0] + new[4] + new[8]
+		diagonal[1] = new[2] + new[4] + new[6]
+		for total in diagonal:
+			if total == 3:
+				self.reset_board()
+				self.win += 1
+			elif total == -3:
+				self.reset_board()
+				self.win -= 1
+
+	def check(self):
+		self.column()
+		self.row()
+		self.diagonal()
+		if self.win == 1:
+			return "Naughts' win"
+		if self.win == -1:
+			return "Crosses' win"
+		else:
+			if self.turn == -1:
+				return "Crosses' Turn"
+			else:
+				return "Naughts' Turn"
+
+	def naught(self,column,row):
+		if self.turn == -1:
+			return "It's not your turn, fuckboy"
+		if column > 3 or row > 3:
+			return "Enter some better co-ordinates."
+		x = column - 1
+		y = row - 1
+		if self.board[y][x] == 0:
+			self.board[y][x] += 1
+			self.turn = -1
+		return self
+
+	def cross(self,column,row):
+		if self.turn == 1:
+			return "It's not your turn, fuckboy"
+		if column > 3 or row > 3:
+			return "Enter some better co-ordinates."
+		x = column - 1
+		y = row - 1
+		if self.board[y][x] == 0:
+			self.board[y][x] -= 1
+			self.turn = 1
+		return self
+		self.check()
+
+	def reset_board(self):
+		self.board = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
+		return
--- bot.py
+++ bot.py
@@ -1,7 +1,13 @@
 import discord
 import random
+from imgurpython import ImgurClient
+import twitter
+import Tictactoe
 
+api = twitter.Api("7L5XmV18ORSnp9yGo1SLuexnL", "ZktX8HqTTFgn25iWvGxizsKovFUWFGVKmacMJ0CrEgTZ6Flmrq", "2799268561-5pXZJUPwTaXm0kFzupPl7jsBDHDbnEpFvy05dE2", "xo9G4HHUswae7z8IdmZ1nzuzvaE1yhUXvkWQb8M7ks2ed")
 client = discord.Client()
+imgurclient = ImgurClient("d0c24d1db4fb537","6d9578660c1ca1dd62c89c5ecc0f8fca289c7467")
+
 
 @client.async_event
 def on_message(message):
@@ -9,40 +15,40 @@ def on_message(message):
 		return
 
 	if message.content.startswith('#test'):
-		yield from client.send_message(message.channel, "This is a fucking test")
+		yield from client.send_message(message.channel, "This is a fucking test", tts = True)
 
 	if message.content.startswith('#frank'):
-		yield from client.send_message(message.channel, "Hey I'm Frank. Would you like to know my opinion?")
+		yield from client.send_message(message.channel, "Hey I'm Frank. Would you like to know my opinion?", tts = True)
 		opinions = ["Hey, I agree with your opinion.", "I'm fucking triggered!", "Hey, your opinion's stupid"]
 		msg = yield from client.wait_for_message(timeout = 10, author = message.author)
 		if msg is None:
-			yield from client.send_message(message.channel, "Wow... I really wanted to share my opinion")
+			yield from client.send_message(message.channel, "Wow... I really wanted to share my opinion", tts = True)
 			return
 		if msg.content == "yes":
 			num = random.randint(0,2)
-			yield from client.send_message(message.channel, opinions[num])
+			yield from client.send_message(message.channel, opinions[num], tts = True)
 		elif msg.content == "no":
-			yield from client.send_message(message.channel, "Umm...Wu")
+			yield from client.send_message(message.channel, "Um. Wu", tts = True)
 		elif msg.content == "wu":
 			while True:
-				yield from client.send_message(message.channel, "Yo...Wu")
+				yield from client.send_message(message.channel, "Yo. Wu", tts = True)
 				msg = yield from client.wait_for_message(timeout = 10, author = message.author,content = "wu") 
 				if msg is None:
-					yield from client.send_message(message.channel, "Wu!")
+					yield from client.send_message(message.channel, "Wu!", tts = True)
 					return
 
 	if message.content.startswith('#choose '):
 		msg = message.content[len('#choose'):].strip()
 		if msg == "":
-			yield from client.send_message(message.channel, "Give me some choices man!")
+			yield from client.send_message(message.channel, "Give me some choices man!", tts = True)
 			return
 		choices = msg.split(",")
 		xs = len(choices)
 		if xs == 1:
-			yield from client.send_message(message.channel, "You only gave me one choice, cunt.")
+			yield from client.send_message(message.channel, "You only gave me one choice, cunt.", tts = True)
 			return
 		num = random.randint(0,xs)
-		yield from client.send_message(message.channel, choices[num])
+		yield from client.send_message(message.channel, "I choose {0}".format(choices[num]), tts = True)
 
 	if message.content.startswith('#kierannudes'):
 		yield from client.send_file(message.channel, "C://bananabot//kieran.jpg", filename = "kieran.jpg", content = "I don't have nudes but I have...")
@@ -51,26 +57,53 @@ def on_message(message):
 		yield from client.send_message(message.channel, "Shutting down...")
 		yield from client.logout()
 
-	if message.content.startswith("#psr "):
-		msg = message.content[len('#psr '):].strip()
+	if message.content.startswith('#imgur '):
+		items = imgurclient.gallery()
+		for item in items:
+			yield from client.send_message(message.channel, item.link)
+			return
+
+	if message.content.startswith('#tictactoe'):
+		t = Tictactoe.Tictactoe()
+		yield from client.send_message(message.channel, t)
+		while True:
+			message = yield from client.wait_for_message()
+			if message.content.startswith('#naught'):
+				coord = message.content.strip("#naught ")
+				row = int(coord[2])
+				column = int(coord[0])
+				yield from client.send_message(message.channel, t.naught(column,row))
+				yield from client.send_message(message.channel, t.check())
+			elif message.content.startswith('#cross'):
+				coord = message.content[len('#cross'):].strip()
+				row = int(coord[2])
+				column = int(coord[0])
+				yield from client.send_message(message.channel, t.cross(column,row))
+				yield from client.send_message(message.channel, t.check())
+			if t.win != 0:
+				return
+
+	if message.content.startswith("#psr"):
+		msg = message.content.strip("psr# ")
 		psr = [":fist:", ":v:", ":raised_hand:"]
 		num = random.randint(0,2)
+		ans = psr[num]
 		if num - 1 == -1:
-			win = 2
+			win = psr[2]
 		else:
-			win = num - 1
+			win = psr[num - 1]
 		if num + 1 == 3:
-			lose = 0
+			lose = psr[0]
 		else:
-			lose = num +1
-		yield from client.send_message(message.channel, psr[num])
-		if msg == psr[num]:
+			lose = psr[num + 1]
+		yield from client.send_message(message.channel, ans)
+		if ans in msg:
 			yield from client.send_message(message.channel, "It's a draw!")
 			return
-		elif msg == psr[win]:
+		elif win in msg:
 			yield from client.send_message(message.channel, "You win!")
 			return
-		elif msg == psr[lose]:
+		elif lose in msg:
 			yield from client.send_message(message.channel, "You lose!")
 			return
 
